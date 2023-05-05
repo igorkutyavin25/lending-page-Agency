cabd06c6d7152fa211a8eb1d43618b1615217bf4
Игорь Кутявин <igorkutyavin@192.168.1.2>
2023-05-05 20:01:00 +0300

    Resposive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('burger').addEventListener('click', function(){
-        document.querySelector('header').classList.toggle('open')
-    })
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('burger').addEventListener('click', function () {
+//         document.querySelector('header').classList.toggle('open')
+//     })
+// });
+
+
+let burgerMenu = document.querySelector('.header__burger-btn');
+let burgerList = document.querySelector('.header');
+
+burgerMenu.addEventListener('click', mobileBurger);
+
+function mobileBurger() {
+    burgerList.classList.toggle('open');
+}
